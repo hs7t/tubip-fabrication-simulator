d11fe69e3d49bbaa95f6a42853f81480ef3a7683
Alí Sinisterra <haidarconh@gmail.com>
2025-09-30 16:51:13 -0500

    add width, height to main on App

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,18 @@
 <script>
+  import GameView from "./lib/GameView.svelte";
 </script>
 
-<main>
+<header>
   <h1>Tubip Fabrication Simulator</h1>
+</header>
+<main>
+  <GameView />
 </main>
 
 <style>
+  main {
+    width: 80%;
+    aspect-ratio: 4 / 3;
+    width: min(100vw, 100vh);
+  }
 </style>
