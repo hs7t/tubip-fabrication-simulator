2672778f142f6fd63a12917f4d019a2516657deb
Alí Sinisterra <haidarconh@gmail.com>
2025-09-30 17:04:34 -0500

    add image to Machine

---
+++ src/lib/Machine.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { ASSETS } from "../assets";
+    let { machineType } = $props()
+    
+    let machineImagePath = $state()
+    let machineImageAlt = $state()
+    switch (machineType) {
+        case "dohlwropMicrowave": 
+            machineImagePath = ASSETS.machines.dohlwrop_microwave
+            machineImageAlt = "A Dohlwrop Microwave machine"
+    }
+</script>
+
+<div class="machine">
+    <img src={machineImagePath} alt={machineImageAlt}>
+</div>
