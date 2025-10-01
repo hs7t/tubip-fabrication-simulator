f6916acb91bcf99241e303cd64e611d5f992012b
Alí Sinisterra <haidarconh@gmail.com>
2025-09-30 19:35:16 -0500

    add NumberInput

---
+++ src/lib/NumberInput.svelte
@@ -0,0 +1,9 @@
+<script>
+    let { id = "number-input", onEnter, value = $bindable() } = $props()
+
+</script>
+
+<div {id}>
+    <input type="number" bind:value={value}>
+    <button onclick={onEnter}>sell tubips</button>
+</div>
