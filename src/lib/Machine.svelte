1b6ce3eb829a6ae18a477d8bb91642041bbb7745
Alí Sinisterra <haidarconh@gmail.com>
2025-09-30 19:41:40 -0500

    remove and update certain code due to scope change

--- src/lib/Machine.svelte
+++ src/lib/Machine.svelte
@@ -1,6 +1,6 @@
 <script>
     import { ASSETS } from "../assets";
-    let { machineType } = $props()
+    let { machineType, onEnter = undefined } = $props()
     
     let machineImagePath = $state()
     let machineImageAlt = $state()
@@ -12,22 +12,8 @@
 </script>
 
 <div class="machine">
-    <button>
-        <img src={machineImagePath} alt={machineImageAlt}>
-    </button>
+    <button onclick={onEnter}>tubip</button>
 </div>
 
 <style>
-    .machine button {
-        width: 100%;
-        height: 100%;
-
-        border: 0;
-        outline: none;
-        background-color: transparent;
-    }
-    .machine button img {
-        width: 100%;
-        height: 100%;
-    }
 </style>
