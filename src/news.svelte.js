40a48b0e10202f28e091cf9332934b82b9381e69
Alí Sinisterra <haidarconh@gmail.com>
2025-10-04 08:12:29 -0500

    add a few generic headlines

---
+++ src/news.svelte.js
@@ -0,0 +1,10 @@
+let genericHeadlines = [
+  {
+    headline: "New film 'Tubips' by acclaimed director sparks tubip craze",
+    priceMultiplier: 1.2,
+  },
+  {
+    headline: "Tubip companies on the rise. Bubble?",
+    priceMultiplier: 0.8,
+  },
+];
