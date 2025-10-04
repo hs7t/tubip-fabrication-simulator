2bb151cdad4ef050b32716293537935897cb7f71
Alí Sinisterra <haidarconh@gmail.com>
2025-10-04 13:53:55 -0500

    make NewsMarquee work

--- src/lib/NewsMarquee.svelte
+++ src/lib/NewsMarquee.svelte
@@ -6,10 +6,45 @@
     <p>{headline}</p>    
 {/snippet}
 
-<div>
-    {#each [1, 2] as scrolled}
-        <span class="scrolling" id="scroller-{scrolled}">
+<div class="marquee-box">
+    {#each [1, 2, 3] as scrolled}
+        {#if scrolled == 1}
+        <span class="scrolling" id="scrolled-{scrolled}">
             {@render content()}
         </span>
+        {:else}
+        <span class="scrolling" id="scrolled-{scrolled}" aria-hidden="true">
+            {@render content()}
+        </span>
+        {/if}
     {/each}
 </div>
+
+<style>
+    .marquee-box {
+        --gap: 1em;
+        display: flex;
+        max-width: 30vw;
+        flex-direction: row;
+        overflow: hidden;
+
+        white-space: nowrap;
+        gap: var(--gap);
+    }
+
+    .marquee-box .scrolling {
+        animation: scroll infinite 4s linear;
+        user-select: none;
+    }
+
+    @keyframes scroll {
+        from {
+            transform: translateX(0);
+        }
+        to {
+            transform: translateX(calc(-100% - var(--gap)));
+        }
+        /* TYSM, Ryan! https://ryanmulligan.dev/blog/css-marquee/ */
+    }
+
+</style>
