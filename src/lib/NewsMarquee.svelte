3bfa051a7ca5567c896265b4956794ea182a0837
Alí Sinisterra <haidarconh@gmail.com>
2025-10-04 13:38:08 -0500

    add NewsMarquee

---
+++ src/lib/NewsMarquee.svelte
@@ -0,0 +1,15 @@
+<script>
+    let { headline } = $props()
+</script>
+
+{#snippet content()}
+    <p>{headline}</p>    
+{/snippet}
+
+<div>
+    {#each [1, 2] as scrolled}
+        <span class="scrolling" id="scroller-{scrolled}">
+            {@render content()}
+        </span>
+    {/each}
+</div>
