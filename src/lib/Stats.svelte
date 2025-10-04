e2ac4dbeee4a324d63fc6a26951661e3373f94eb
Alí Sinisterra <haidarconh@gmail.com>
2025-10-04 14:45:04 -0500

    style stats

--- src/lib/Stats.svelte
+++ src/lib/Stats.svelte
@@ -5,9 +5,21 @@
 
 <div class="stats">
     {#each ["coins", "tubips", "matter"] as stat}
-    <p>{stat}:</p>
-    {#key game.state[stat]}
-        <p transition:slide>{game.state[stat]}</p>
-    {/key}
+    <div class="stat">
+        <p>{stat}:</p>
+        {#key game.state[stat]}
+            <p transition:slide>{game.state[stat]}</p>
+        {/key}
+    </div>
     {/each}
 </div>
+
+<style> 
+    .stats {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        width: clamp(0px, 100%, 500px);
+    }
+</style>
