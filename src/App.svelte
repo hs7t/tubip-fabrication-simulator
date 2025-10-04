63e6a1685fe1097542a0a8ad21f674da58e35bd1
Alí Sinisterra <haidarconh@gmail.com>
2025-10-04 14:34:51 -0500

    update width, flex direction

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import NumberInput from "./lib/NumberInput.svelte";
   import { game, saveGameToLocalStorage, handleTubipGeneration, handleTubipSale } from "./gamestate.svelte.js";
   import './tick.svelte.js'
+  import './news.svelte.js'
   import NewsMarquee from "./lib/NewsMarquee.svelte";
   import Stats from "./lib/Stats.svelte";
 
@@ -28,11 +29,10 @@
     background-color: inherit;
     padding: 5%;
 
-    min-width: 100%;
+    width: clamp(280px, 100%, 1000px);
 
     display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
+    flex-direction: column;
     gap: 1rem;
   }
 
