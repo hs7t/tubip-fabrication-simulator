290d5579893f2a2123c6d86c28c05bf448d37663
Alí Sinisterra <haidarconh@gmail.com>
2025-10-05 08:40:47 -0500

    add basic shop things

--- src/lib/Shop.svelte
+++ src/lib/Shop.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { items } from "../shop.svelte.js";
+    import { handleItemPurchase, items } from "../shop.svelte.js";
 
     let shopItems = $derived(arrayFromObject(items))
 
@@ -13,10 +13,32 @@
     }
 </script>
 
-<div>
-    <ul>
+<div class="shop">
+    <div class="items">
         {#each shopItems as item}
-            <li>{item.name}</li>
+            <button class="item" onclick={() => handleItemPurchase(item.id)}>
+                <p class="name">{item.name}</p>
+            </button>
         {/each}
-    </ul>
+    </div>
 </div>
+
+<style> 
+    .shop {
+        width: 100%;
+    }
+    .items {
+        display: flex;
+        flex-direction: column;
+        gap: 0.3rem;
+    }
+    .item {
+        all: unset;
+        cursor: pointer;
+        padding: 0.8ch;
+        border: var(--t-border-primary);
+    }
+    .item .name {
+        font-weight: 600;
+    }
+</style>
