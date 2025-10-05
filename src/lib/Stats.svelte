f7d08130066120664c6e95d923904db3bb1643ac
Alí Sinisterra <haidarconh@gmail.com>
2025-10-05 08:24:43 -0500

    add classes for different types of display

--- src/lib/Stats.svelte
+++ src/lib/Stats.svelte
@@ -2,26 +2,37 @@
     import { game } from "../gamestate.svelte";
     import { slide } from "svelte/transition";
 
-    let { statsShown = ["coins", "tubip", "matter"] } = $props()
+    let { statsShown = ["coins", "tubip", "matter"], kind = "adapt" } = $props()
 </script>
 
-<div class="stats">
+<div class="stats" class:adapt={kind=="adapt"} class:compact={kind=="compact"}>
     {#each statsShown as stat}
     <div class="stat">
         <p>{stat}:</p>
         {#key game.state[stat]}
-            <p transition:slide>{game.state[stat]}</p>
+            <p class="number" transition:slide>{game.state[stat]}</p>
         {/key}
     </div>
     {/each}
 </div>
 
-<style> 
+<style>
     .stats {
         display: flex;
+    }
+    .stats.adapt {
         flex-direction: row;
         justify-content: space-between;
         flex-wrap: wrap;
         width: clamp(0px, 100%, 500px);
+    }
+    .stats.compact {
+        display: flex;
+        flex-direction: column;
+    }
+    .stats.compact .stat {
+        display: flex;
+        flex-direction: row;
+        gap: 1ch;
     }
 </style>
