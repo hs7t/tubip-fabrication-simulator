5059f078e5c539b3efc12b6752a5736329034838
Alí Sinisterra <haidarconh@gmail.com>
2025-10-05 07:23:46 -0500

    add ShopDialog component

---
+++ src/lib/ShopDialog.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Dialog from "./components/Dialog.svelte"
+
+    let { shown = $bindable(false) } = $props()
+    function makeShown(bool) {
+        shown = bool;
+    }
+</script>
+
+{#snippet content()}
+    <button onclick={() => makeShown(false)}>Close</button>
+    <h2>meow</h2>
+{/snippet}
+
+<Dialog {content} bind:shown={shown}></Dialog>
