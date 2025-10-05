56e7e747abd760ba56ed12a2e177a5a2da163625
Alí Sinisterra <haidarconh@gmail.com>
2025-10-05 08:24:55 -0500

    show stats

--- src/lib/SaleDialog.svelte
+++ src/lib/SaleDialog.svelte
@@ -2,12 +2,14 @@
     import Dialog from "./components/Dialog.svelte"
     import NumberInput from "./components/NumberInput.svelte";
     import { handleTubipSale } from "../gamestate.svelte";
+  import Stats from "./Stats.svelte";
 
     let { shown = $bindable(false) } = $props()
 </script>
 
 {#snippet content()}
-    <NumberInput buttonLabel="sell tubip" onEnter={(response) => { handleTubipSale(response) }} allowNegatives={false}></NumberInput>
+    <Stats statsShown={["coins", "tubip"]} kind={"compact"}></Stats>
+    <NumberInput buttonLabel="Sell tubip" onEnter={(response) => { handleTubipSale(response) }} allowNegatives={false}></NumberInput>
 {/snippet}
 
-<Dialog {content} title="Market" bind:shown={shown}></Dialog>
+<Dialog {content} title="Sell" bind:shown={shown}></Dialog>
