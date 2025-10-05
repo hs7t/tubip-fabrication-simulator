d415f77c34ec4a8fde369448f7ba4408393e4df4
Alí Sinisterra <haidarconh@gmail.com>
2025-10-05 13:23:30 -0500

    write HelpDialog

---
+++ src/lib/HelpDialog.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Dialog from "./components/Dialog.svelte";
+    let { open = true } = $props();
+</script>
+
+{#snippet content()}
+<ul>
+    <li>Hit the <b>FABRICATE</b> button to fabricate tubip.</li>
+    <li>Tubip matter is needed for fabrication. You'll get some every so often - watch the counters!</li>
+    <li>You can sell tubip for coins on the Market.</li>
+    <li>Coins can be used to buy upgrades that might help you fabricate more tubip.</li>
+    <li>Keep an eye on the news ticker! Efficiency and exchange rates fluctuate based on recent events (and also randomly).</li>
+</ul>
+{/snippet}
+
+<Dialog {content} shown={open}></Dialog>
