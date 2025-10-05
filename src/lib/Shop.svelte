4d8f1610545ce98010d80d39644a4a5e19c78e23
Alí Sinisterra <haidarconh@gmail.com>
2025-10-04 19:00:23 -0500

    load items in Shop

--- src/lib/Shop.svelte
+++ src/lib/Shop.svelte
@@ -1,3 +1,14 @@
 <script>
     import { items } from "../shop.svelte.js";
+
+    let shopItems = $derived(arrayFromObject(items))
+
+    function arrayFromObject (object) {
+        let objectItems = []
+        for (let objectItemKey of objectItems.keys()) {
+            shopItems.push(items[objectItems]);
+            // TODO: I'm too tired to figure out how to convert an object to an array efficiently.
+        }
+        return objectItems;
+    }
 </script>
